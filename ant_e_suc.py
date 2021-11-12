285b611fa71a0838d280f3eadcca83b494a4e631
Contreira-C <caioperes2001@gmail.com>
2021-11-12 18:27:36 -0300

    mult.py

---
+++ ant_e_suc.py
@@ -0,0 +1,5 @@
+inteiro = (int(input("Insira um número inteiro: ")))
+inteiro_ant = inteiro - 1
+inteiro_suc = inteiro + 1
+print("O número anterior a {} é {}." .format(inteiro,inteiro_ant))
+print("O número sucessor a {} é {}." .format(inteiro,inteiro_suc))
---
+++ mult.py
@@ -0,0 +1,9 @@
+import math
+inteiro = (int(input("Insira um número inteiro: ")))
+mult =  inteiro * 2
+cubo = inteiro * inteiro * inteiro
+quad = math.sqrt(inteiro)
+
+print("O dobro de {} é {}".format(inteiro,mult))
+print("O cubo de {} é {}".format(inteiro, cubo))
+print("A raiz de {} é {}".format(inteiro, quad))
